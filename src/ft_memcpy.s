7c9db3e1990bfeae15ac535eade836f189fbfeb4
Alaric Senat <asenat@student.42.fr>
2018-11-15 11:50:31 +0100

    moving dword by dword in memcpy

--- src/ft_memcpy.s
+++ src/ft_memcpy.s
@@ -2,10 +2,23 @@ section .text
 global _ft_memcpy
 
 _ft_memcpy:
-	mov rax, rdi	; we will return the base ptr
+;	mov rax, rdi	; we will return the base ptr
+	mov r8, rdi
 
-	mov rcx, rdx	; length given in parameter
-	cld
+	mov rax, rdx	;
+	xor rdx, rdx	;
+	mov rbx, 0x4	;
+	div rbx			; div rax with rbx, quotient ->rax, remainder -> rdx
+
+	cld				; clear  right direction flag
+
+	; first we cpy the trailing bytes 1 by 1
+	mov rcx, rdx	; remainder of the div will be the length of the bytes to cpy
 	rep movsb		; copy the content of rsi in rdi until rcx is 0
 
+	; then we cpy dwords by dwords
+	mov rcx, rax	; quotient of the div will be the length of the dwords to cpy
+	rep movsd		; copy the content of rsi in rdi until rcx is 0
+
+	mov rax, r8
 	ret				; return rax
--- tests/ft_memcpy_memmove.c
+++ tests/ft_memcpy_memmove.c
@@ -4,6 +4,8 @@
 #include <strings.h>
 #include <stdlib.h>
 
+#define BIG_STR "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
 void *ft_memcpy(void *b, void const *, size_t len);
 void *ft_memmove(void *b, void const *, size_t len);
 
@@ -14,13 +16,14 @@ typedef struct {
 	const char *name;
 } test_data_t;
 
-test_state_t single_test(const char *source, size_t s, const test_data_t *data) {
-	char s1[s];
-	char s2[s];
-	memcpy(s1, source, s);
-	memcpy(s2, source, s);
-	char* ref = data->ref(s1, source, s);
-  	char* ret = data->to_test(s2, source, s);
+test_state_t single_test(const char *source, size_t s, size_t to_cpy, const test_data_t *data) {
+	char s1[s + 1];
+	char s2[s + 1];
+	memset(s1, 'q', s);
+	memset(s2, 'q', s);
+	s1[s] = s2[s] = 0;
+	char* ref = data->ref(s1, source, to_cpy);
+  	char* ret = data->to_test(s2, source, to_cpy);
 	if (ref != s1 || ret != s2 || memcmp(s1, s2, s)) {
 		dprintf(2, "[FAIL] %1$s(%2$.10s) -> %3$p, %4$s != ft_%1$s(%2$.10s) -> %5$p, %6$s\n",
 				data->name, source, ref, s1, ret, s2);
@@ -33,13 +36,19 @@ test_state_t single_test(const char *source, size_t s, const test_data_t *data) 
 test_state_t run_test(const test_data_t *data) {
 	test_state_t ret = TEST_SUCCESS;
 	
-	if (single_test("123456", 6, data) == TEST_FAIL)
+	if (single_test("123456", 6, 6, data) == TEST_FAIL)
 		ret = TEST_FAIL;
-	if (single_test("123\0 56", 7, data) == TEST_FAIL)
+	if (single_test("123456", 6, 3, data) == TEST_FAIL)
 		ret = TEST_FAIL;
-	if (single_test("123", 0, data) == TEST_FAIL)
+	if (single_test("123\0 56", 7, 7, data) == TEST_FAIL)
 		ret = TEST_FAIL;
-	if (single_test("", 0, data) == TEST_FAIL)
+	if (single_test(BIG_STR, 128, 128, data) == TEST_FAIL)
+		ret = TEST_FAIL;
+	if (single_test(BIG_STR, 128, 103, data) == TEST_FAIL)
+		ret = TEST_FAIL;
+	if (single_test("123", 3, 0, data) == TEST_FAIL)
+		ret = TEST_FAIL;
+	if (single_test("", 0, 0, data) == TEST_FAIL)
 		ret = TEST_FAIL;
 	return ret;
 }
