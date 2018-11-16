68ef397533f1e51e69e586fc5c5f55a7abac22dc
Alaric Senat <asenat@student.42.fr>
2018-11-16 18:09:10 +0100

    ft_puts refactor

--- src/ft_puts.s
+++ src/ft_puts.s
@@ -1,4 +1,14 @@
+%macro write_str 0
+	mov rax, 0x2000004
+	mov rdi, 0x1
+	syscall
+	jc .error
+%endmacro
+
 extern _ft_strlen
+
+section .rodata
+	null_msg: db "(null)", 0ah
 
 section	.text
 	global _ft_puts
@@ -7,23 +17,21 @@ _ft_puts:
 	push rbp
 	mov rbp, rsp
 	sub rsp, 0x8			; creating the stack frame
-	
+
 	cmp rdi, 0x0			; if the string is null we jump to the display_null routine
 	je .display_null
 
 	mov [rbp-0x8], rdi		; save the base ptr in the stack
 
 	call _ft_strlen			;  we get the string len in rax
-	
+
 	mov rdx, rax			; rdx contains the len of the string
 	mov rsi, [rbp-0x8]		; rsi contains a ptr to the string
-	call write_s			; do the syscall
-	jc .error				; if carry flag is set, return err
+	write_str 				; do the syscall
 
-	lea rsi, [rel line_break]	; just pick the \n of null_msg...
-	mov rdx, 0x1			
-	call write_s			; ...and write it
-	jc .error				; if carry flag is set, return err
+	lea rsi, [rel null_msg+0x6]	; just pick the \n of null_msg...
+	mov rdx, 0x1
+	write_str				; ...and write it
 
 	.success:
 		mov rax, 0xa		; puts return 10 on success
@@ -31,22 +39,12 @@ _ft_puts:
 	.error:
 		mov rax, -1
 	.end:
-		mov rsp, rbp		; destroy the stack frame and ret 
-     	pop rbp	
+		mov rsp, rbp		; destroy the stack frame and ret
+		pop rbp
 		ret					; return rax
 
 	.display_null: 			; This part juste write "(null)\n" and return
 		mov rdx, 0x7
 		lea rsi, [rel null_msg]
-		call write_s
-		jc .error			; if carry flag is set, return err
+		write_str
 		jmp .success
-data:
-	null_msg: db "(null)", 0ah
-	line_break: db 0ah
-
-write_s:
-	mov rax, 0x2000004
-	mov rdi, 0x1
-	syscall
-	ret
