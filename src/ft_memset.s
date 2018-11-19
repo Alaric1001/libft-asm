48838a603f625319f1cb9138b0d03150b31224f5
Alaric Senat <asenat@student.42.fr>
2018-11-19 13:30:17 +0100

    Prefer not using the stack when not needed

--- src/ft_memset.s
+++ src/ft_memset.s
@@ -2,18 +2,12 @@ section .text
 global _ft_memset
 
 _ft_memset:
-	push rbp
-	mov rbp, rsp
-	sub rsp, 0x8		; creating a 8bytes stack frame
-
-	mov [rbp-0x8], rdi	; saving rdi in the stack to return it
+	mov r8, rdi			; saving rdi in the r8 register
 
 	mov al, sil			; put the char to copy in al
 	mov rsi, rdi		; source string (dest string is rdi)
 	mov rcx, rdx		; length of the string
 	rep stosb			; copy the content of al in rdi until rcx is 0
 
-	mov rax, [rbp-0x8]	; we will return the base pt
-	mov rsp, rbp		; destroy the stack frame and ret 
-    pop rbp	
+	mov rax, r8			; we will return the base pt
 	ret				; return rax
